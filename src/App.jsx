f20ebfeadf39569c3327b9f2f89eb645fe1d0685
maru0210 <m.ryuto0210@gmail.com>
2024-06-26 17:10:10 +0900

    add: card(nirocon), footerの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './App.css'
-import Footer from './components/Footer'
+import "./App.css";
+import Footer from "./components/Footer";
+import { Card_nirocon } from "./components/Card_nirocon";
 
 function App() {
   return (
@@ -7,10 +8,18 @@ function App() {
       {/* TODO ヘッダーを作る */}
 
       {/* メイン */}
+      <Card_nirocon
+        img_link={
+          "https://raw.githubusercontent.com/nitgc-densan-club/tmp/main/no_image.jpg"
+        }
+        title={"テスト"}
+        text={"テストテキスト"}
+      />
 
       {/* TODO フッターを作る */}
-      <Footer></Footer>          </>
-  )
+      <Footer></Footer>
+    </>
+  );
 }
 
-export default App
+export default App;
